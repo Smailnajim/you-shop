519ab8afb9f7fa7789bb40499b04d26d29a5414b
smailnajim <smailnajim2003@gmail.com>
2025-12-25 10:08:19 +0100

    create resource and update auth project  to work with "TCP"

--- apps/authentification/src/main.ts
+++ apps/authentification/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AuthentificationModule } from './authentification.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AuthentificationModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AuthentificationModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: 3001,
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
