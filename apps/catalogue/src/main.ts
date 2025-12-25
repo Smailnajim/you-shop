6904342bedd1ff31f5a403bad1eca39998c9ebee
smailnajim <smailnajim2003@gmail.com>
2025-12-25 10:12:02 +0100

    update project Catalogue to work by "TCP"

--- apps/catalogue/src/main.ts
+++ apps/catalogue/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { CatalogueModule } from './catalogue.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(CatalogueModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    CatalogueModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: 3002,
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
