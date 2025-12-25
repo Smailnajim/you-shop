ceafa2c836451e2dabf802104bd40bfb782770cf
smailnajim <smailnajim2003@gmail.com>
2025-12-25 10:36:58 +0100

    create Catalogue gateway

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { GatewayController } from './gateway.controller';
 import { GatewayService } from './gateway.service';
 import { AuthModule } from './auth/auth.module';
+import { CatalogueModule } from './catalogue/catalogue.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, CatalogueModule],
   controllers: [GatewayController],
   providers: [GatewayService],
 })
