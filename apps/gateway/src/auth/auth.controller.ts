4777eb2b772c67c4ae4d53642f214a649967d054
smailnajim <smailnajim2003@gmail.com>
2025-12-30 21:45:39 +0100

    install "@nestjs/passport" &  test run auth service and test a request.

--- apps/gateway/src/auth/auth.controller.ts
+++ apps/gateway/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('users')
@@ -6,7 +6,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/login')
-  Login() {
-    return 'login';
+  Login(@Body() loginDto: any) { 
+    return this.authService.login(loginDto);
   }
 }
--- apps/gateway/src/auth/auth.service.ts
+++ apps/gateway/src/auth/auth.service.ts
@@ -4,4 +4,8 @@ import { ClientProxy } from '@nestjs/microservices';
 @Injectable()
 export class AuthService {
   constructor(@Inject('AUTH_CLIENT') private authClient: ClientProxy) {}
+
+  login(loginDto: any) {
+    return this.authClient.send('login', loginDto);
+  }
 }
