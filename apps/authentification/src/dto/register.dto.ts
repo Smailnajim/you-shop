3d485e1197c6e30f80d1f617b72eaba1617f33ee
smailnajim <smailnajim2003@gmail.com>
2026-01-02 15:46:00 +0100

    add name to schema and migrate

--- apps/authentification/src/dto/register.dto.ts
+++ apps/authentification/src/dto/register.dto.ts
@@ -1,23 +1,20 @@
-import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
-
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class RegisterDto {
-    @IsEmail({}, {message: 'Email is not valid'})
-    @IsNotEmpty({message: 'Email is required'})
-    email: string;
+  @IsEmail({}, { message: 'Email is not valid' })
+  @IsNotEmpty({ message: 'Email is required' })
+  email: string;
 
-    @IsString()
-    @MinLength(6, {message: 'Password must be at least 6 characters long'})
-    @IsNotEmpty({message: 'Password is required'})
-    password: string;
+  @IsString()
+  @MinLength(6, { message: 'Password must be at least 6 characters long' })
+  @IsNotEmpty({ message: 'Password is required' })
+  password: string;
 
-    @IsString()
-    @IsNotEmpty({message: 'First name is required'})
-    firstName: string;
+  @IsString()
+  @IsNotEmpty({ message: 'First name is required' })
+  firstName: string;
 
-    @IsString()
-    @IsNotEmpty({message: 'Last name is required'})
-    lastName: string;
-    
+  @IsString()
+  @IsNotEmpty({ message: 'Last name is required' })
+  lastName: string;
 }
-    
